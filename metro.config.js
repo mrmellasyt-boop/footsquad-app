5aa44a394fc61ff14f78c539c47c27f650918087
Mrmellas <mrmellasyt-boop@users.noreply.github.com>
2026-02-28 13:16:50 -0500

    Fix metro.config.js watchFolders for Vercel build

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+// Ensure node_modules cache directories are watched (fixes Vercel/CI build)
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(__dirname, "node_modules"),
+];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
